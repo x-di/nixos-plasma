7245deadebbde069f6941c407f6625bf63c7b56f
x-di <dimasimam75@gmail.com>
2024-05-07 11:16:30 +0700

    add locale

--- nixos/system/locale/default.nix
+++ nixos/system/locale/default.nix
@@ -1,9 +1,15 @@
 { ... }:
 let
   defaultlocale = "en_US.UTF-8";
+  extralocale = "id_ID.UTF-8";
 in
 {
   i18n = {
     defaultLocale = "${defaultlocale}";
+    extraLocaleSettings = {
+      LANGUAGE = "${defaultlocale}";
+      LC_ALL = "${extralocale}";
+      #LC_TIME = "${extralocale}";
+    };
   };
 }

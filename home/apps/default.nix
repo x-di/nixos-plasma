17de0362853530ecd0a4f9a03b27403206a75f46
x-di <dimasimam75@gmail.com>
2024-05-15 14:46:36 +0700

    remove bun, go, nodejs, php, composer - use nix-shell

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -2,26 +2,27 @@
 {
   home.packages = with pkgs; [
     #brave
-    bun
+    #bun
     clinfo
     #dia
     drawio
     fastfetch
     glxinfo
     gnome.gnome-keyring
-    go
+    #go
     #gruvbox-plus-icons
     haruna
     #helix
     htop
+    kdePackages.kdeplasma-addons
     libdbusmenu
     #libreoffice-qt-fresh
-    #nodejs_20
-    nodejs_22
+    #nodejs_20 #nix-shell -p nodejs --command fish
+    #nodejs_22
     onlyoffice-bin_latest
     p7zip
-    php
-    php82Packages.composer
+    #php #nix-shell -p php php82Packages.composer --command fish
+    #php82Packages.composer
     postman
     sqlite
     tela-icon-theme

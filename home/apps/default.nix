96f8eea1fc7f8e0b52c73587cd55dd11bf2ebbf2
x-di <dimasimam75@gmail.com>
2024-05-17 04:00:45 +0700

    remove unused packages

--- home/apps/default.nix
+++ home/apps/default.nix
@@ -1,29 +1,18 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    #brave
-    #bun
     clinfo
-    #dia
     drawio
     fastfetch
     glxinfo
     gnome.gnome-keyring
-    #go
-    #gruvbox-plus-icons
     haruna
-    #helix
     htop
     kdePackages.kdeplasma-addons
     libdbusmenu
-    #libreoffice-qt-fresh
-    #nodejs_20 #nix-shell -p nodejs --command fish
-    #nodejs_22
     obs-studio
     onlyoffice-bin_latest
     p7zip
-    #php #nix-shell -p php php82Packages.composer --command fish
-    #php82Packages.composer
     postman
     sqlite
     tela-icon-theme
@@ -31,7 +20,5 @@
     thunderbird
     vscode-fhs
     vulkan-tools
-    yt-dlp
-    #zed-editor
   ];
 }
